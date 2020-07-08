2622ea581b2c511ee0833ef548e83a816d386809
Vi <62430555+ViVeesio@users.noreply.github.com>
2020-07-08 13:02:15 +0200

    Add files via upload
    Command Handler's Update

---
+++ commands/Utilities/ping.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+
+    message.channel.send(`Pong !`);
+
+}
+
+exports.help = {
+    name: "ping",
+    aliases: ['pong']
+}
--- index.js
+++ index.js
@@ -4,14 +4,30 @@ const bot = new Discord.Client();
 
 let prefix = "!";
 
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+const loadCommands = require("./functions/commands.js");
+
+const load = async () => {
+    await loadCommands.run(bot);
+}
+
 bot.on('ready', () => {
     console.log(`${bot.user.username} is ready !`);
 });
 
 bot.on('message', message => {
-    if(message.content === `${prefix}ping`){
-        message.channel.send("Pong !!");
-    }
+    
+    const args = message.content.split(/ +/g);
+    const command = args.shift().slice(prefix.length).toLowerCase();
+    const cmd = bot.commands.get(command) || bot.aliases.get(command);
+
+    if(!message.content.toLowerCase().startsWith(prefix) || !message.guild || message.author.bot || !cmd) return;
+
+    cmd.run(bot, message, args).catch(e => {return console.log(e)});
+
 });
 
+load();
 bot.login(config.token);
